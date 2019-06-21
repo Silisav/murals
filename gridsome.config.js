adefed11a46747360e5a65e35c2dfeae1f8a9698
Vasilis <vasilis.tsirimokos@on-running.com>
2019-06-21 21:19:15 +0200

    Add gridsome airtable plugin

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,10 +1,21 @@
-// This is where project configuration and plugin options are located. 
+// This is where project configuration and plugin options are located.
 // Learn more: https://gridsome.org/docs/config
 
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
-}
+  siteName: "Murals Berlin",
+  plugins: [
+    {
+      use: "@gridsome/source-airtable",
+      options: {
+        apiKey: process.env.AIRTABLE_KEY,
+        baseId: process.env.AIRTABLE_BASE,
+        tableName: "Murals",
+        typeName: "Mural",
+        route: "/murals/:path"
+      }
+    }
+  ]
+};
