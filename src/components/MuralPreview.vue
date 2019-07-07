6381a523094a6e51c68030005c586986effc3174
Vasilis <vasilis.tsirimokos@on-running.com>
2019-07-07 12:47:01 +0200

    Add mural preview component

---
+++ src/components/MuralPreview.vue
@@ -0,0 +1,30 @@
+<template>
+  <article>
+    <g-link :to="mural.path">
+      <figure v-if="image">
+        <g-image :src="image.url" :alt="image.name" />
+      </figure>
+    </g-link>
+  </article>
+</template>
+
+<script>
+export default {
+  name: "MuralPreview",
+  props: ["mural"],
+
+  data() {
+    return {
+      image: this.mural.photos.length
+        ? {
+            name: this.mural.photos[0].filename,
+            url: this.mural.photos[0].thumbnails.large.url
+          }
+        : null
+    };
+  }
+};
+</script>
+
+<style scoped>
+</style>
