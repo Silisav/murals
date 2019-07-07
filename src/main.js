6c9e93c85ec74b2281b784358937767cefb61615
Vasilis <vasilis.tsirimokos@on-running.com>
2019-07-07 12:44:11 +0200

    Import masonry and leaflet plugins

--- src/main.js
+++ src/main.js
@@ -1,9 +1,26 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
-import DefaultLayout from '~/layouts/Default.vue'
+import DefaultLayout from "~/layouts/Default.vue";
+import { VueMasonryPlugin } from "vue-masonry";
 
-export default function (Vue, { router, head, isClient }) {
+import { LMap, LTileLayer, LMarker } from "vue2-leaflet";
+import { Icon } from "leaflet";
+import "leaflet/dist/leaflet.css";
+
+delete Icon.Default.prototype._getIconUrl;
+
+Icon.Default.mergeOptions({
+  iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+  iconUrl: require("leaflet/dist/images/marker-icon.png"),
+  shadowUrl: require("leaflet/dist/images/marker-shadow.png")
+});
+
+export default function(Vue, { router, head, isClient }) {
   // Set default layout as a global component
-  Vue.component('Layout', DefaultLayout)
+  Vue.component("Layout", DefaultLayout);
+  Vue.use(VueMasonryPlugin);
+  Vue.component("l-map", LMap);
+  Vue.component("l-tile-layer", LTileLayer);
+  Vue.component("l-marker", LMarker);
 }
