3c894f1409ddb50162e1af9eff46b9ca0514ca7a
Vasilis <vasilis.tsirimokos@on-running.com>
2019-08-05 22:40:17 +0200

    Fetch and use artist website and location data

--- src/templates/Mural.vue
+++ src/templates/Mural.vue
@@ -2,9 +2,13 @@
   <transition name="fade" appear>
     <Layout>
       <section class="main">
+        <span>TITLE</span>
         <h1>{{ $page.mural.title }}</h1>
 
-        <p>{{ $page.mural.artist }}</p>
+        <span>ARTIST</span>
+        <h2><span v-html="artist"></span></h2>
+
+        <p>{{ $page.mural.address }} <span v-html="gmapsLink"></span></p>
         <aside>
           <figure>
             <g-image
@@ -32,16 +36,48 @@ figure,
 .fade-enter {
   opacity: 0;
 }
+
+h1,
+h2 {
+  margin-top: 5px;
+}
 </style>
 
 
 
 <script>
 export default {
+  name: "Mural",
+
   metaInfo() {
     return {
       title: this.$page.mural.title
     };
+  },
+
+  computed: {
+    artist() {
+      return this.$page.mural.website
+        ? `<a href="${this.$page.mural.website}" target="_blank">${this.$page.mural.artist}</a>`
+        : this.$page.mural.artist;
+    },
+
+    gmapsLink() {
+      return this.$page.mural.latLong
+        ? `<a href="https://www.google.com/maps/search/?api=1&query=${
+            this.getCoordinates(this.$page.mural.latLong).latitude
+          },${
+            this.getCoordinates(this.$page.mural.latLong).longitude
+          }" target="_blank"><br/>(View on Google maps)</a>`
+        : "";
+    }
+  },
+
+  methods: {
+    getCoordinates(coordinates) {
+      const [latitude, longitude] = coordinates.split(",");
+      return { latitude, longitude };
+    }
   }
 };
 </script>
@@ -53,6 +89,9 @@ query Mural ($path: String!) {
     title,
     path,
     artist,
+    website,
+    address,
+    latLong,
     photos {
       thumbnails {
          full{
